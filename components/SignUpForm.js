e2525622da0d25766dcd7089760214f0bd7204a6
Zac McClung <zac.mcclung370@gmail.com>
2018-05-31 11:20:33 -0600

    debugging with async/await

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -8,15 +8,6 @@ const ROOT_URL = 'https://us-central1-one-time-password-21950.cloudfunctions.net
 class SignUpForm extends Component {
   state = { phone: '' };
 
-  handleSubmit = async () => {
-    try {
-      await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });
-      await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   // ^^^^^^^ Same as vvvvvvvvvvvvv
   // handleSubmit = () => {
   //   axios
@@ -30,6 +21,15 @@ class SignUpForm extends Component {
   //        console.log(err)
   //     })
   // };
+
+  handleSubmit = async () => {
+    try {
+      await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone });
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
 
   render() {
     return (
